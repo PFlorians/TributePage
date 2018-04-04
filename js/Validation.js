9a34b16ad6732442ff9a6bbc0df02bdd382cf12f
PFlorians <patrikflorians@gmail.com>
2018-04-04 10:14:25 +0200

    On branch noscript-ajax  Your branch is up to date with 'html3/noscript-ajax'.  Changes to be committed: 	modified:   html/dynamic-content/about-body.html 	modified:   html/dynamic-content/amiga-body.html 	modified:   html/dynamic-content/index-body.html 	modified:   html/dynamic-content/prince-body.html 	modified:   html/dynamic-content/software-body.html 	modified:   index.html 	modified:   js/Validation.js 	new file:   js/vue.js

--- js/Validation.js
+++ js/Validation.js
@@ -1,8 +1,9 @@
 //validatory
+var pass1="";
 function validateName(src)
 {
 	let inp=src.value;
-	if(/^[A-Z]{1}[a-z]+$/.test(String(inp)) == false)
+	if(/^[a-zA-Z0-9]+$/.test(String(inp)) == false)
 	{
 		$("#chybaUname").removeClass("hide");
 	}
@@ -18,12 +19,32 @@ function validateMail(src)
 function validatePasswdChars(src)
 {
 	let inp=src.value;
+	pass1=src.value;
+	console.log("chars called");
 	if(/[a-zA-Z0-9\.\\\/\-\,\?\!\_\]\[]/.test(String(inp)) == false)
 	{
-		
+		$("#passwd1Err").removeClass("hide");
+	}
+	else
+	{
+		console.log("validne");
 	}
 }
-
+function validatePasswdMatch(src)
+{
+	let inp = src.value;
+	if(pass1==inp)
+	{
+		if(/[a-zA-Z0-9\.\\\/\-\,\?\!\_\]\[]/.test(String(inp)) == false)
+		{
+			$("#passwd2Err").removeClass("hide");
+		}
+	}
+	else
+	{
+		$("#passwd2ErrMatch").removeClass("hide");
+	}
+}
 //utils
 function hideUnameErr()
 {
@@ -32,4 +53,31 @@ function hideUnameErr()
 function hideMailErr()
 {
 	$("#chybaMail").addClass("hide");
+}
+function hidePassw1Err()
+{
+	$("#passwd1Err").addClass("hide");
+}
+function hidePassw2Err()
+{
+	console.log("match: " + $("#passwd2ErrMatch").hasClass("hide") + " chars: " + $("#passwd2Err").hasClass("hide"));
+	if($("#passwd2Err").hasClass("hide"))
+	{
+		if($("#passwd2ErrMatch").hasClass("hide")==false)
+		{
+			$("#passwd2ErrMatch").addClass("hide");
+		}
+	}
+	else
+	{
+		if($("#passwd2ErrMatch").hasClass("hide"))
+		{
+			$("#passwd2Err").addClass("hide");	
+		}	
+		else//realne by nemalo nastat ale keby nieco
+		{
+			$("#passwd2Err").addClass("hide");
+			$("#passwd2ErrMatch").addClass("hide");
+		}
+	}
 }
